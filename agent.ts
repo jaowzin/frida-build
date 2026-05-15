643aea882aa5747acaff10e2d109b8c36b523561
jaowzin <joaofelipesantos99944@gmail.com>
2026-05-15 01:27:54 -0300

    bot: update agent.ts 1778819274314-b6cd5f

--- agent.ts
+++ agent.ts
@@ -1,5 +1,16 @@
 import "frida-il2cpp-bridge";
 
+const classes = ["Rocket"];
+const methods = [".cctor"]
+
 Il2Cpp.perform(() => {
-    console.log(`Hello, Unity ${Il2Cpp.unityVersion}`);
+  console.log(Il2Cpp.unityVersion);
+  setTimeout(() => {
+    Il2Cpp.trace()
+      .assemblies(Il2Cpp.Domain.assembly("Assembly-CSharp"))
+      .filterClasses(cls => classes.includes(cls.name))
+      .filterMethods(mtd => methods.includes(mtd.name))
+      .and()
+      .attach("detailed");
+  }, 30 * 1000); // we sleep 30 seconds so the application doesn't crash on attach
 });
