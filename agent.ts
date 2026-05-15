e671034064c38ed314a61651b04ff8faf6af5086
jaowzin <joaofelipesantos99944@gmail.com>
2026-05-14 22:58:16 -0300

    bot: update agent.ts 1778810295654-90b077

--- agent.ts
+++ agent.ts
@@ -1,52 +1,18 @@
 import "frida-il2cpp-bridge";
 
-const TARGET_ASSEMBLY = "Assembly-CSharp";
-
-console.log("[+] Trace Assembly-CSharp iniciado");
+console.log("[+] Script IL2CPP bridge carregado");
 
 Il2Cpp.perform(() => {
-  const assembly = Il2Cpp.domain.assembly(TARGET_ASSEMBLY);
+  console.log("[+] Il2Cpp inicializado");
+  console.log("[+] Unity version: " + Il2Cpp.unityVersion);
 
-  if (assembly == null) {
-    console.log("[-] Assembly-CSharp nao encontrada");
+  const assemblies = Il2Cpp.domain.assemblies;
+  console.log("[+] Assemblies encontradas: " + assemblies.length);
 
-    console.log("[*] Assemblies disponiveis:");
-    for (const asm of Il2Cpp.domain.assemblies) {
-      console.log(" - " + asm.image.name);
-    }
+  assemblies.slice(0, 10).forEach((assembly, index) => {
+    const image = assembly.image;
+    console.log(`[${index}] Assembly: ${image.name} | Classes: ${image.classes.length}`);
+  });
 
-    return;
-  }
-
-  const image = assembly.image;
-
-  console.log("[+] Assembly encontrada: " + image.name);
-  console.log("[+] Classes: " + image.classes.length);
-
-  let totalHooks = 0;
-
-  for (const klass of image.classes) {
-    for (const method of klass.methods) {
-      try {
-        if (method.virtualAddress.isNull()) {
-          continue;
-        }
-
-        const fullName = `${klass.name}.${method.name}`;
-
-        Interceptor.attach(method.virtualAddress, {
-          onEnter(args) {
-            console.log("[CALL] " + fullName);
-          }
-        });
-
-        totalHooks++;
-      } catch (e) {
-        console.log("[-] Erro em " + klass.name + "." + method.name + ": " + e);
-      }
-    }
-  }
-
-  console.log("[+] Trace instalado");
-  console.log("[+] Total de metodos hookados: " + totalHooks);
+  console.log("[+] Teste finalizado sem hookar/modificar nada");
 });
